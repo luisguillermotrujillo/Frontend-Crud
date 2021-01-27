6d5a8fb6a7208f674f99bd9f3d36bcdb22131c8e
luis gullermo <luisguillermotrujillo16@gmail.com>
2021-01-26 21:37:14 -0500

    conectando frontend con backend

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
     fetch("http://localhost:5000/getAll")
     .then(response => response.json())
-    .then(data => console.log(data));
-    loadHTMLtable([])
+    .then(data => loadHTMLtable(data["data"]))
+    
+});
+const addBtn = document.gelElementById("add-Name-btn");
+
+addBtn.addEventListener("click", function () {
+    const nameImput = document.gelElementById("name-input");
+    const name = nameImput.value;
+    nameImput.value = "";
+
+    fetch("http://localhost:5000/insert",{
+        headers:{
+            'content-type':'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({name: name})
+    })
+    .then(response => response.json())
+    .then(data=>)
 });
 
 function loadHTMLtable(data) {
     const table = document.querySelector("table tbody");
+    console.log(data);
 
     if (data.length === 0) {
         table.innerHTML = "<tr><td class='no-data' colspan = '5'>No-Data</td></tr>";

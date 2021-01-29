4ff099531c62f0c2e04c259b63ce4d788a0018db
luis gullermo <luisguillermotrujillo16@gmail.com>
2021-01-28 21:01:58 -0500

    conectando backend con frontend

--- index.js
+++ index.js
@@ -30,6 +30,19 @@ function loadHTMLtable(data) {
     console.log(data);
 
     if (data.length === 0) {
-        table.innerHTML = "<tr><td class='no-data' colspan = '5'>No-Data</td></tr>";
+        table.innerHTML = "<tr><td class =  'no-data' colspan = '5'>No-Data</td></tr>";
+        return;
     }
+    let tableHtml = "";
+
+    data.forEach(function({id,Name,date_added}) {
+        tableHtml += "<tr>";
+        tableHtml += `<td> ${id} </td>`;
+        tableHtml += `<td> ${Name} </td>`;
+        tableHtml += `<td> ${new Date(date_added).toLocaleString()} </td>`;
+        tableHtml += `<td> <button class = "delete-ron-btn" data-id = ${id}> Delete </td>`;
+        tableHtml += `<td><button class = "edit-row-btn" data-id = ${id}>Edit </td>`;
+        tableHtml += "</tr> "
+    });
+    table.innerHTML = tableHtml;
 }
